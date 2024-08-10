232f27090615c7e1659cad3aa36f088907c7ec17
starburst-xhl <126219732+starburst-xhl@users.noreply.github.com>
2024-08-10 18:07:02 +0800

    loader换为m3

--- src/main/kotlin/components/MyLoader.kt
+++ src/main/kotlin/components/MyLoader.kt
@@ -4,10 +4,10 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.LinearProgressIndicator
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -37,8 +37,10 @@ fun myLoader(loadingStatus: LoadingStatus) {
                                 modifier = Modifier.size(50.dp)
                             )
                             LoadingStatus.LoaderType.Linear -> LinearProgressIndicator(
+                                progress = {
+                                    loadingStatus.getProgress() / 100f
+                                },
                                 modifier = Modifier.size(200.dp, 4.dp),
-                                progress = loadingStatus.getProgress() / 100f
                             )
                         }
                     }
